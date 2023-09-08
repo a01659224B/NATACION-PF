26c685c35d8ec53b60d30ab35f4efacf251341fb
vaniamga <vaniagallardo578@gmail.com>
2023-09-08 15:51:42 -0600

    conoce mas

---
+++ src/components/botones.vue
@@ -0,0 +1,22 @@
+<template>
+    
+    <div id="app">
+        
+        <button @click="cambiarColor" :style="{ backgroundColor: botonColor }">Conoce Más </button>
+    </div>
+</template>
+    <script>
+        new Vue({
+            el: '#app',
+            data: {
+                botonColor: 'yellow'
+            },
+            methods: {
+                cambiarColor: function() {
+                    
+                    const colores = ['#ff5733', '#ffde33', '#33ff57', '#337aff', '#ff33c7'];
+                    const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+                    this.botonColor = colorAleatorio;
+                }
+            }
+        });
