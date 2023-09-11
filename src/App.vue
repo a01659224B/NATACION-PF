f54c5ddd0baa9f47a523a5ce8bf7cd52b4c31a77
vaniamga <vaniagallardo578@gmail.com>
2023-09-11 12:28:36 -0600

    conoce mas

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script>
 import Encabezado from './components/Encabezado.vue';
+import MyButton from './components/Boton.vue';
+export default {
+    components: {
+      'my-button': MyButton,
+    },
+  };
 </script>
 
 <template>
@@ -7,7 +13,13 @@ import Encabezado from './components/Encabezado.vue';
   <!-- <router-link to="/">Adultos</router-link>
   <router-link to="/">Bebe</router-link>
   <router-link to="/">Ninos</router-link> -->
+  
+    <div>
+      <!-- Otras partes de tu vista -->
+      <my-button></my-button>
+    </div>
   </template>
+  
   
   
   <style></style>
