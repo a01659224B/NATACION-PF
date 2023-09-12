af648a9cc5c43e81a67a6ff606d18d97ee439fc2
vaniamga <vaniagallardo578@gmail.com>
2023-09-11 18:14:37 -0600

    cambios

---
+++ src/components/cardsCu.vue
@@ -0,0 +1,31 @@
+<div class="container text-center">
+    <div class="row align-items-center">
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+    <img src="src/components/imagenes/22221495_1494986850591554_5286114235300830434_n.jpg" class="card-img-top" alt="...">
+    <div class="card-body text-bg-primary text-center">
+    <h5 class="card-title">Natacion Niños</h5>
+    <a href="#" class="btn btn-warning">Conoce mas</a>
+    </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card" style="width: 18rem;">
+    <img src="src/components/imagenes/21728814_1473610312729208_566200209273822402_o.jpg" class="card-img-top" alt="...">
+    <div class="card-body text-bg-primary text-center">
+    <h5 class="card-title">Natacion Adultos</h5>
+    <a href="#" class="btn btn-warning">Conoce mas</a>
+    </div>
+</div>
+    </div>
+    <div class="col">
+        <div class="card col" style="width: 18rem;">
+    <img src="src/components/imagenes/273209402_4785210898235783_6528893329297105951_n.jpg" class="card-img-top" alt="...">
+    <div class="card-body text-bg-primary text-center">
+    <h5 class="card-title">Natacion Bebes</h5>
+    <a href="#" class="btn btn-warning">Conoce mas</a>
+    </div>
+        </div>
+    </div>
+    </div>
+</div>
---
+++ src/components/footer.vue
@@ -0,0 +1,72 @@
+<template>
+    <footer class="footer">
+        <div class="footer-border-top"></div> <!-- Línea blanca superior -->
+        <div class="footer-logo">
+            <!-- Agrega aquí tu logo -->
+            <img src="src/components/imagenes/na-pn (1).png" alt="Logo" />
+        </div>
+        <div class="footer-social">
+            <!-- Enlaces a las redes sociales -->
+            <p>REDES SOCIALES</p>
+            <a href="https://t.me/tucanal" target="_blank"><i class="fab fa-telegram"></i></a>
+            <a href="https://wa.me/tunumero" target="_blank"><i class="fab fa-whatsapp"></i></a>
+            <a href="https://www.facebook.com/tupagina" target="_blank"><i class="fab fa-facebook"></i></a>
+        </div>
+        <div class="footer-links">
+            <p>UBICACION</p>
+            <router-link to="/ubicacion">Av. Tonaltecas 2650, Arboledas de San Gaspar, 45410 Tonalá, Jal.</router-link>
+        </div>
+        <div class="footer-border-bottom"></div> <!-- Línea blanca inferior -->
+        <p>Naatare - © 2023 Todos los derechos reservados</p>
+    </footer>
+    </template>
+    
+    <style scoped>
+    .footer {
+        background-color: #3771C8; /* Color azul */
+        color: #fff;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px 20px;
+        position: relative; /* Permite posicionar las líneas */
+    }
+    
+    .footer-logo img {
+        max-height: 40px;
+    }
+    
+    .footer-social a {
+        color: #fff;
+        margin-right: 10px;
+        text-decoration: none;
+        font-size: 20px;
+    }
+    
+    .footer-links a {
+        color: #fff;
+        text-decoration: none;
+        font-size: 16px;
+    }
+    
+    p{
+        color:#EBE741
+        
+    }
+    .footer-border-top,
+    .footer-border-bottom {
+        height: 2px;
+        background-color: #fff; /* Color blanco */
+        position: absolute;
+        left: 0;
+        right: 0;
+    }
+    
+    .footer-border-top {
+        top: 0;
+    }
+    
+    .footer-border-bottom {
+        bottom: 0;
+    }
+    </style>
