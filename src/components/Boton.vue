e1279244ec0fd73c7f967c1da6e8d15f1bdf917d
SakayHT <amandiuxy@gmail.com>
2023-09-12 11:31:13 -0500

    botones vania

--- src/components/Boton.vue
+++ src/components/Boton.vue
@@ -1,24 +1,39 @@
-<template>
-    
-    <div id="app">
-        
-        <button @click="cambiarColor" :style="{ backgroundColor: botonColor }">Conoce Más </button>
-    </div>
-</template>
     <script>
-        new Vue({
-            el: '#app',
-            data: {
-                botonColor: 'yellow'
-            },
-            methods: {
-                cambiarColor: function() {
-                    
-                    const colores = ['#ff5733', '#ffde33', '#33ff57', '#337aff', '#ff33c7'];
-                    const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-                    this.botonColor = colorAleatorio;
-                }
-            }
-        });
-
-        
+    export default {
+    data() {
+        return {
+        buttonText: "Conoce mas",
+        clicked: false
+        };
+    },
+    
+    };
+    </script>
+    <template>
+        <button
+        class="custom-btn"
+        @click="handleClick"
+        >
+        {{ buttonText }}
+        </button>
+    </template>
+    
+    <style scoped>
+    .custom-btn {
+        background-color: #EBE741; /* Color púrpura personalizado */
+        color: #000; /* Color de texto negro */
+        border: none;
+        border-radius: 4px;
+        padding: 10px 20px;
+        cursor: pointer;
+        transition: background-color 0.3s ease-in-out; /* Transición suave del color de fondo */
+    }
+    
+    .custom-btn:hover {
+        background-color: #9D1C9E; /* Cambiar a rosa al pasar el mouse sobre el botón */
+    }
+    
+    .custom-btn:focus {
+        outline: none; /* Eliminar el contorno al hacer clic en el botón */
+    }
+    </style>
---
+++ src/components/BotonPrecio.vue
@@ -0,0 +1,39 @@
+    <script>
+    export default {
+    data() {
+        return {
+        buttonText: "Precio",
+        clicked: false
+        };
+    },
+    
+    };
+    </script>
+    <template>
+        <button
+        class="custom-btn"
+        @click="handleClick"
+        >
+        {{ buttonText }}
+        </button>
+    </template>
+    
+    <style scoped>
+    .custom-btn {
+        background-color: #EBE741; /* Color púrpura personalizado */
+        color: #000; /* Color de texto negro */
+        border: none;
+        border-radius: 4px;
+        padding: 10px 20px;
+        cursor: pointer;
+        transition: background-color 0.3s ease-in-out; /* Transición suave del color de fondo */
+    }
+    
+    .custom-btn:hover {
+        background-color: #9D1C9E; /* Cambiar a rosa al pasar el mouse sobre el botón */
+    }
+    
+    .custom-btn:focus {
+        outline: none; /* Eliminar el contorno al hacer clic en el botón */
+    }
+    </style>
