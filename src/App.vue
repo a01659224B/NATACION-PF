a0de74db43270418598279293322260132165b8c
Dulce Velázquez <dulcekarinavb@outlook.com>
2023-09-13 00:47:43 -0600

    tablas

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script>
 import Encabezado from './components/Encabezado.vue'
-import LoadingSpinner from './components/icons/LoadingSpinner.vue'
+//import LoadingSpinner from './components/icons/LoadingSpinner.vue'
 
-let loading = ref(true)
+//let loading = ref(true)
 </script> 
 
-<template>  
+<template> 
 <!-- <LoadingSpinner v-if="loading"/>  -->
 <!-- <router-link to="/">Adultos</router-link>
   <router-link to="/">Bebe</router-link>
   <router-link to="/">Ninos</router-link> -->
   </template>
+
+
