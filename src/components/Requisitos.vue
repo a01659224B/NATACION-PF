2d06f3811a0b4c00621c3f3226bbc7c0648fe79d
vaniamga <vaniagallardo578@gmail.com>
2023-09-16 22:29:01 -0600

    .

--- src/components/Requisitos.vue
+++ src/components/Requisitos.vue
@@ -2,47 +2,57 @@
 </script>
 
 <template>
-<div class="container text-center cuadro">
-  <div class="row align-items-center">
-      <div class="col cuadroUno">
-      <h3>Niños</h3>
-      <h5>4 a 13 años de edad clase 30 minutos</h5>
+     
+  <div class="container text-center mt-4 cuadro">
+    <div class="row align-items-center">
+      <div class="col cuadroUno p-4">
+        <h3>Niños</h3>
+        <h5>4 a 13 años de edad clase 30 minutos</h5>
       </div>
-      <div class="col cuadroDos">
-      <h3>Adultos</h3>
-      <h5>14 años en adelante clase 30 minutos</h5>
+      <div class="col cuadroDos p-4">
+        <h3>Adultos</h3>
+        <h5>14 años en adelante clase 30 minutos</h5>
       </div>
-      <div class="col cuadroTres">
-      <h3>Bebes</h3>
-      <h5>4 meses a 3 años clase 30 minutos</h5>
+      <div class="col cuadroTres p-4">
+        <h3>Bebes</h3>
+        <h5>4 meses a 3 años clase 30 minutos</h5>
       </div>
-  </div>
-  
+    </div>
 
-  <div>
-      <ul>
-          <li>Certificado medico (institucion publica o particular)</li>
-          <li>Gogles (para natacion)</li>
-          <li>Traje de baño ( damas una pieza)</li>
-          <li>Sandalias ( para uso en regaderas)</li>
-          <li>Toalla</li>
-          <li>Bebes pañal para el agua cuando no controla esfiner</li>
+    <div class="mt-4">
+      <div class="cuadror">
+        <h3 class="text-center">Requisitos</h3>
+      </div>
+      <ul class="list-unstyled" style="padding-left: 20px;">
+        <li>Certificado médico (institución pública o particular)</li>
+        <li>Goggles (para natación)</li>
+        <li>Traje de baño (damas una pieza)</li>
+        <li>Sandalias (para uso en regaderas)</li>
+        <li>Toalla</li>
+        <li>Bebes pañal para el agua cuando no controla esfínter</li>
       </ul>
+    </div>
   </div>
-</div>
 </template>
 
 <style>
-.cuadro{
-    background-color:#EBE741 ;
-}
-.cuadroUno{
+  .cuadro {
+    background-color: #EBE741;
+  }
+  .cuadroUno {
     background-color: #3C469E;
-}
-.cuadroDos{
+  }
+  .cuadroDos {
     background-color: #3C469E;
-}
-.cuadroTres{
+  }
+  .cuadroTres {
     background-color: #3C469E;
-}
+  }
+  .cuadror{
+    font-family:  'Montserrat', sans-serif;
+    color:#3C469E;
+    text-decoration: none;
+    
+  }
+
 </style>
